80907fff1af3705e7f57c1520273b2a6341107d5
hermann <s.hermann@billiger-mietwagen.de>
2025-02-19 13:21:23 +0100

    Add single install script and update readme

--- install_powerline.sh
+++ install_powerline.sh
@@ -4,7 +4,6 @@ set -eux pipefail
 # Install Powerline for VIM.
 sudo apt install -y python3-pip pipx
 pipx install powerline-status
-sudo cp configs/.vimrc ~/.vimrc
 sudo apt install -y fonts-powerline
 
 # Install Patched Font
